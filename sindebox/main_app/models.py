f8b4955918091432e29e8018e51d3d9eac59526e
arnozal <arnozal@us.es>
2024-10-23 12:55:22 +0200

    First commit. EnergyData model created and tested by using postman

---
+++ sindebox/main_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+import uuid
+from datetime import datetime
+
+# Create your models here.
+class EnergyData(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    voltage = models.FloatField(default=0)
+    current = models.FloatField(default=0)
+    power = models.FloatField(default=0)
+    timestamp = models.DateTimeField(default=datetime.now)
+    
---
+++ sindebox/main_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from .models import EnergyData
+import json
+
+
+@csrf_exempt
+def main_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        EnergyData.objects.create(
+            voltage=data['voltage'],
+            current=data['current'],
+            power=data['power']
+        )
+        return JsonResponse({'status': 'success'})
+    return render(request, 'main_app/main_view.html')
+
