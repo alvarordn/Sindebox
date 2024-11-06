771b52ba0bf962a2b2d83ceb7040ae91512eaeab
Alvaro Rodriguez Del Nozal <alvarorn@hotmail.com>
2024-11-06 13:14:45 +0100

    basic auth

--- sindebox/main_app/views.py
+++ sindebox/main_app/views.py
@@ -1,42 +1,43 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseForbidden
+from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 from .models import EnergyData
 import json
-from django.conf import settings
-from urllib.parse import urlparse
-
+from base64 import b64decode
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
+def basic_auth(request):
+    auth_header = request.headers.get('Authorization', None)
+    if auth_header is None or not auth_header.startswith('Basic '):
+        return None        
+    auth_value = auth_header[6:]  
+    decoded_value = b64decode(auth_value).decode('utf-8')  
+    username, password = decoded_value.split(':')
+    return username, password
 
 @csrf_exempt
 @api_view(['POST'])
 def my_shelly(request):
-    url = request.build_absolute_uri()
-    
-    parsed_url = urlparse(url)
-    auth = parsed_url.netloc.split('@')[0]  
-    
-    if ':' not in auth:
-        return HttpResponseForbidden("No authentication credentials found in URL")    
-    username, password = auth.split(':')
-
-    if username != 'sindetec' or password != settings.SHELLY_PASSWORD:
+    username, password = basic_auth(request)
+    if username is None or password is None:
+        return HttpResponseForbidden("No authentication credentials found")
+    if username != "sindetec" or password != "macarena":
         return HttpResponseForbidden("Invalid authentication credentials")
-
     try:
         request_body = request.body.decode('utf-8')
         data = json.loads(request_body)
     except (json.JSONDecodeError, UnicodeDecodeError) as e:
         return HttpResponseForbidden(f"Invalid JSON or encoding: {str(e)}")
 
-    EnergyData.objects.create(
-        voltage=data.get('voltage', 0),
-        current=data.get('current', 0),
-        power=data.get('power', 0)
-    )
 
+    data = json.loads(request.body)
+    EnergyData.objects.create(
+        voltage=data['voltage'],
+        current=data['current'],
+        power=data['power']
+    )
     return JsonResponse({'status': 'success'})
 
 @api_view(['POST'])
